7d9c13da253c9d02e9760aac31f58fa96fd6dea7
Hagai Zwick <hagai.zwick@gmail.com>
2015-11-22 13:25:35 -0800

    s

---
+++ week-2/zwick-reflection.m
@@ -0,0 +1,15 @@
+What git concepts were you struggling with prior to the GPS session?
+
+It is hard for me to put my finger on a specific concept, but the all idea of the connection between the terminal to the Github site was a little off for me. now, after I had experienced using many time with ‘pull’ ,’merge’,’touch’ I understand how it’s work
+
+What concepts were clarified during the GPS?
+I was clarified about the ‘push origin branch name’ ,now I understand the logic behind that. I know that it is possible to push only after your computer ‘pull’ before, and that’s what makes u and the master on the same page.
+		
+What questions did you ask your pair and the guide?
+I had technical question regarding on how to execute the challenge. I understand before the challenge how it works, now I not only understand the general concept, I know exactly how it works.
+
+What still confuses you about git?
+One thing that’s still trick me is when you go from your terminal to Github, u can’t link on any directory, it have to be a specific directory( add list that’s what I think …?) vs linking any directory to github whenever u want.
+
+How was your first experience of pairing in a GPS?
+It was nice and I liked it. It really help by making thing more clear. two is always better then one, that’s what I think, and in the peer meeting, it is an opportunity to learn from one another.
