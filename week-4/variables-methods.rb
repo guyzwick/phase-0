5b35e668e77122d9e7569e2f3ffa142831afe11b
Hagai Zwick <hagai.zwick@gmail.com>
2015-12-05 10:42:38 -0800

    lodaing Rucy files

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,11 @@
+puts 'Hello there, and what\'s your name?'
+name = gets. chomp
+puts 'Your name is  ' + name + '? What a Lovely name!'
+puts 'Pleased to meet you, ' + name + '.  :-)'
+
+
+puts 'Hello there, What\'s is you favorite number ?'
+number = gets.chomp
+new_number = number.to_i + 1
+puts 'How about, ' + new_number.to_s + ' It\'s bigger and better number'
+
