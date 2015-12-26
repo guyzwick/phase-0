de1caf641f3b9a42a822008cd8ffede236f43eca
Hagai Zwick <hagai.zwick@gmail.com>
2015-12-26 12:12:50 -0800

    calculate-grade

---
+++ week-4/4.5/calculate-grade/my_solution.rb
@@ -0,0 +1,22 @@
+# Calculate a Grade
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+def get_grade(num)
+   if num >= 90 
+	grade = "A"
+elsif num >= 80
+	grade = "B"
+elsif num >= 70
+	grade = "C"
+elsif num >= 60
+	grade = "D"
+else
+	grade = "F"
+end
+	end
+
+
+
