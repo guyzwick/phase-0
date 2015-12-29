faab2cadc25634fba13092bea80742857a84c534
Hagai Zwick <hagai.zwick@gmail.com>
2015-12-28 18:16:32 -0800

    4.6.6

--- week-4/4.6/count-between/my_solution.rb
+++ week-4/4.6/count-between/my_solution.rb
@@ -15,5 +15,9 @@
 # Your Solution Below
 
 def count_between(list_of_integers, lower_bound, upper_bound)
-    list_of_integers.count{|x| x >= lower_bound && x <= upper_bound}
+  if (list_of_integers).size == 0
+    return 0
+  else
+    p list_of_integers.count { |x| x.between?( lower_bound, upper_bound) }
+  end
 end
