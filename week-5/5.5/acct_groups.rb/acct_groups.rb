7450e1bdfb4724bda96e2647ec086b5b795d127e
Hagai Zwick <hagai.zwick@gmail.com>
2016-01-09 15:12:23 -0800

    adding reflactions

--- week-5/5.5/acct_groups.rb/acct_groups.rb
+++ week-5/5.5/acct_groups.rb/acct_groups.rb
@@ -36,3 +36,24 @@ def pepole_list(name)
   end
 end
 p pepole_list(['Hagai', 'John', 'Yoko', 'Mandi', 'Aeron', 'Gorege', 'Harison'])
+
+=begin
+
+What was the most interesting and most difficult part of this challenge?
+The most interesting part was to find the ‘slice’ method, which I think is pretty cool. The hardest and most annoying part was the part of the math that you had to think about in order to make the group in the right size. It was definitely a tricky one ( just like all the test) but, I got there.
+
+Do you feel you are improving in your ability to write pseudocode and break the problem down?
+Yes. Practice makes perfect, and like all things in life, also my pseudocode code writing is getting better. Actually, I should say that my way of looking into a problem and braking it to pieces is getting better, and that is the heart of the pseudocode.
+
+Was your approach for automating this task a good solution? What could have made it even better?
+Well, I think my approach was good. Of course that modification needed to be made and I had to solve more problem that I first thought. The general direction was good.
+
+What data structure did you decide to store the accountability groups in and why?
+I decided to use an Array. I thought that since the names came in array form and there is no need to organize them by key, all you looked is how to sort the element in the array in the right way, so array was perfect for this. You shouldn't over complicate things if you don't need to.
+
+What did you learn in the process of refactoring your initial solution? Did you learn any new Ruby methods?
+I this this challenge with the help of the teachers, so, I didn't saw any way I could refactor My code. To be honest, I feel i need to have more experience in order to refactor my code better.
+
+=end
+
+
