3049079fd2155c38d6eceb0f6de091963b223937
Hagai Zwick <hagai.zwick@gmail.com>
2016-01-13 17:05:53 -0800

    die class

--- week-6/die-2/die_spec.rb
+++ week-6/die-2/die_spec.rb
@@ -1,62 +1,51 @@
 require_relative 'my_solution'
 
-describe GuessingGame do
-  let(:game) { GuessingGame.new(10) }
-
+describe Die do
   describe '#initialize' do
-    it "expects a single argument" do
-      expect(GuessingGame.instance_method(:initialize).arity).to eq 1
+    it 'expects a single argument' do
+      expect(Die.instance_method(:initialize).arity).to eq 1
+    end
+
+    it 'raises an ArgumentError if list of labels is empty' do
+      expect { Die.new([]) }.to raise_error(ArgumentError)
     end
   end
 
-  describe '#guess' do
-    it "is defined" do
-      expect(GuessingGame.method_defined?(:guess)).to be true
+  describe '#sides' do
+    it 'expects no arguments' do
+      expect(Die.instance_method(:sides).arity).to be_zero
     end
 
-    it "expects a single argument" do
-      expect(GuessingGame.instance_method(:guess).arity).to eq 1
-    end
+    it 'returns the number of sides given on initialization' do
+      length = 1 + rand(100)
+      sides = Array.new(length) { 'A' }
+      die = Die.new(sides)
 
-    it "returns :high when the guess is too high" do
-      expect(game.guess(100)).to eq :high
-    end
-
-    it "returns :low when the guess is too low" do
-      expect(game.guess(0)).to eq :low
-    end
-
-    it "returns :correct when the guess is correct" do
-      expect(game.guess(10)).to eq :correct
-    end
-
-    it "changes solved? when a correct guess is made" do
-      expect { game.guess(10) }.to change(game, :solved?).from(false).to(true)
-    end
-
-    it "doesn't change solved? when an incorrect guess is made" do
-      expect { game.guess(5) }.to_not change(game, :solved?).from(false)
-    end
-
-    it "reflects the last guess" do
-      # This guess is correct, so game.solved? should be true
-      game.guess(10)
-
-      expect { game.guess(5) }.to change(game, :solved?).from(true).to(false)
+      expect(die.sides).to eq length
     end
   end
 
-  describe '#solved?' do
-    it "is defined" do
-      expect(GuessingGame.method_defined?(:solved?)).to be true
+
+  describe '#roll' do
+    it 'expects no arguments' do
+      expect(Die.instance_method(:roll).arity).to be_zero
     end
 
-    it "expects no arguments" do
-      expect(GuessingGame.instance_method(:solved?).arity).to be_zero
+    it 'returns a single letter if one label is passed in' do
+      random_letter = ('A'..'Z').to_a[rand(26)]
+      die = Die.new([random_letter])
+
+      expect(Array.new(100) { die.roll }).to eq Array.new(100) { random_letter }
     end
 
-    it "returns false before a guess is made" do
-      expect(game).to_not be_solved
+    it 'returns every possible letter for a sufficiently large number of rolls' do
+      possible_values = ('A'..'Z').to_a.shuffle.first(15)
+
+      die = Die.new(possible_values)
+
+      output = Array.new(10000) { die.roll }.uniq
+
+      expect(output.sort).to eq possible_values.sort
     end
   end
 end
--- week-6/die-2/my_solution.rb
+++ week-6/die-2/my_solution.rb
@@ -1,73 +1,75 @@
-# input:
-# 10
-# output:
-# "
-# game.solved?   # => false
-
-# game.guess(5)  # => :low
-# game.guess(20) # => :high
-# game.solved?   # => false
-
-# game.guess(10) # => :correct
-# game.solved?   # => true
-# "
-
-# class Dog
-  
-#   def initialize(breed)
-#     @breed
-#   end
-  
-#   def bark
-#     puts 'woof woof'
-#   end
-  
-#   def command(your_command)
-#     puts "I don't have to do your command #{your_command}"
-#   end
-# edn
-
-class GuessingGame
-  
-  def initialize(answer)
-    @answer = answer
-  end
-  
-  def guess(guess)
-    @guess = guess
-    if guess == @answer
-      :correct
-    elsif guess < @answer 
-      :low
-    else
-      :high
+class Die
+  def initialize(side_array)
+    @side_array = side_array
+    if side_array.empty?
+      raise ArgumentError
     end
   end
   
-  def solved?
-    @answer == @guess
+  def sides
+    @side_array.size
   end
   
-  def last_guess
-  end
-  
+  def roll
+    @side_array[rand(@side_array.size)]
+  end  
 end
 
-=begin
+die = Die.new(['A', 'B', 'C', 'D', 'E', 'F'])
 
-How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object?
-This challenge demonstrated in a nice way how method and characteristics can be used to solve a very nice old game. In this challenge we saw how the method means a real variable that implicated the different answer that one can get on the old gassing game 
+# die.sides
+# p die.roll
+# #<Die:0x00000001e620e8>
+# die.sides
 
-When should you use instance variables? What do they do for you?
-instance variables are the building blocks inside the class methods, we use them in order to spread the information inside the class. They live not only in the specific method, they live in the general class, and by doing this, they can share their information in more places as needed inside the class.
+# # still returns the number of sides, in this case 6
+# p die.roll # returns one of ['A', 'B', 'C', 'D', 'E', 'F'] => 'A' || 'B' || 'C'
 
-Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what did you struggle with?
-The flow control is much easier in classes. Work can be more efficient, and have more possibilities way then just with different method that are floating around. In order to use a certain variable, by defining it as a instance variable, you can switch it around different method. This way the general class and information flow is much better, since there is more information that is being shared.
+# input:
+# geting an array with number of elemnt
+# raise an error if the array is empty
+# output:
+# the sides method will return the number of sides for the die
+# the roll method will return a random letter from the die  
+
+#   steps:
+#   1.creating a Die class that acpet an array
+#     2.Creating if method that checks if the argument is diffrent from 0 
+#     2.1 - if yes , it should return an array alart
+#     2.2 if no, countiue normally.
+#       3.creat a side method the count how many elments are in the array and rreturn the final number.
+#       4creat a roll method that randomly return a one number or a string in the array rang.
+
+# class Dog
+#   def initialize(name)
+#     @name = name
+#     puts "HAI I AM DOGE"
+#   end  
+  
+#   def say_my_name
+#     puts "My name is #{@name}"
+#   end
+# end
 
 
-Why do you think this code requires you to return symbols? What are the benefits of using symbols?
-I think that this code requiring me to return symbols because it demonstrated the result of the method in the best possible way, and this is also answer the second question, if you have a better way to share and say your idea, go ahead and do it, which is excexly what happen in this case.
+# my_dog = Dog.new('Ace')
+# my_dog.say_my_name
 
+=begin 
 
-		
+What were the main differences between this die class and the last one you created in terms of implementation? Did you need to change much logic to get this to work?
+
+The main difference were that this one was easier to make, since having one big system running in one class, making thing easier to run. I also felt that on die 1 ‘side’ was relevant, which is in die 2 it wasn't really necessary, you can run the code fine and get a random string from your index even without knowing how many side, element, are there.
+
+What does this exercise teach you about making code that is easily changeable or modifiable? 
+
+I think that it all about the variables in the class, because once you don't have only local variable and you have one big system that combine all the information, it just make it easy. Let say like this: imagine that instead of no borders between the 50 states, there were border between each stat, and every time you need to cross, or send food or something you need to fill form, or stand in line. Won't that make thing a bit more complicated ? It will .Another thing is since it easy to define variables, it is also easy to change them.
+
+What new methods did you learn when working on this challenge, if any?
+
+I learn how to define how to write the string in the element by picking the index in the array. That was cool. Beside that, wasn't anything new.
+
+What concepts about classes were you able to solidify in this challenge?
+I had to remind myself how classes in general work. It's been awhile since I worked on this in Codecademy, so making a class, using an initialize method helped. Also I getting used again the in a class, toy can have an empty method, or method with no arguments.
+
 =end
