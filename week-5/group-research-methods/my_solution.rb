6041ed1f2e618b49b0875ecdc9cf506d5c1ea09b
Hagai Zwick <hagai.zwick@gmail.com>
2016-01-15 18:01:29 -0800

    credit card

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -7,42 +7,50 @@ my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, 
 
 # Person 1's solution
 def my_array_finding_method(source, thing_to_find)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.select {|item| item.to_s[thing_to_find]}
 end
 
 def my_hash_finding_method(source, thing_to_find)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.select {|key, value| value == thing_to_find}.keys
 end
 
-# Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
 
 # Person 2
 def my_array_modification_method!(source, thing_to_modify)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+ source.map! {|i| i.is_a?(Integer) ? (i + thing_to_modify) : i} 
 end
+
 
 def my_hash_modification_method!(source, thing_to_modify)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
-end
+  source.each {|k,v| source[k] = v + thing_to_modify}
+ end
+
 
 # Identify and describe the Ruby method(s) you implemented.
-#
+ # array = source
+  #hash = my_family_pets_ages
+  #x = thing_to_modify
+
+# source.include? integers? if so each integer needs to be + thing_to_find
+# return source
+
+# Identify and describe the Ruby method(s) you implemented.
+## Method to update the quantity of an item
+# input: hash of items and quantities
+# steps: update quantity (value)
+# output: revised shopping list
 #
 #
 
 
 # Person 3
 def my_array_sorting_method(source)
-  source.map!{ |ele| ele.to_s}.sort 
+   source.sort_by {|x| x.to_s }
 end
 
 def my_hash_sorting_method(source)
    source.sort_by { |key, value| value } 
 end
-
 
 # Identify and describe the Ruby method(s) you implemented.
 #
@@ -52,13 +60,13 @@ end
 
 # Person 4
 def my_array_deletion_method!(source, thing_to_delete)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.reject! {|item| item.to_s.include?(thing_to_delete) }
 end
 
 def my_hash_deletion_method!(source, thing_to_delete)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+   source.delete(thing_to_delete)
+   source
 end
-
 # Identify and describe the Ruby method(s) you implemented.
 #
 #
@@ -67,12 +75,17 @@ end
 
 # Person 5
 def my_array_splitting_method(source)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.partition { |v| v.is_a?(Integer)}
 end
 
 def my_hash_splitting_method(source, age)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  array = []
+  source.each do |name, value|
+    array << [name, value]
+  end
+  array.partition { |v| v[1] <= age}
 end
+
 
 # Identify and describe the Ruby method(s) you implemented.
 #
--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -116,7 +116,14 @@ end
 
 =begin
 
+1)What was the most difficult part of this challenge for you and your pair?
+1)I think that two thing kind of threw us back. The first is that the first thing that crossed your mind in how to compare credit card is different in how was needed in this challenge, so this kind of threw me off in the beginning. And what really was annoying, is the fact that in the initel instructions it’s written that we should build a method, and only later on in the instructions it's said that it should be a class, so that got us mad and made us reorganize and build out method into  class. In the end class was better, but if we had known on that from the beginning it would be much better.
 
+2)What new methods did you find to help you when you refactored?
+2)we used the ‘each_with_index’, we actually find it in the refactor stage, it definitely make thing shorter and faster. Beside that we didn't used any thing special or new.
+
+3)What concepts or learnings were you able to solidify in this challenge?
+3)I think that what this challenge really clear is different concept and method to ‘play’ with the numbers and how to know in a very deep concept how to perform hard tesl with the numbers: double the second from the last, then separate the two digit number, then to flatten the number and to combine it together. Just to write it it takes allot, to write the code for this wasn't easy
  
 =end
 
